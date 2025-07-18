0f4bc8611f23ba59ad28ff8108cd15a0588bf055
Waldthies <98609197+Waldthies@users.noreply.github.com>
2025-07-18 13:21:47 +0200

    Black_Jack_Blueprint

--- 6-OOP/Python/OOP1.py
+++ 6-OOP/Python/OOP1.py
@@ -1,3 +1,7 @@
+
+
+# Koordinaten und Formen
+
 # Schreiben Sie eine Klasse "Coordinate", die man unter Angabe des X- und Y-Wert erstellen kann.
 # Außerdem soll die Klasse eine passende Ausgabe werfen, wenn "print(my_coordinate)" aufgerufen wird
 
@@ -57,5 +61,107 @@ class Rectangle:
                 return (distances[0]**2 == distances[1]**2 + distances[2]**2)
         
        
-          
+       
+# Black Jack
 
+# Schreiben Sie eine Klasse "card", die die Felder "value" und "suit" besitzt.
+
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit 
+    def __str__(self):
+        return str((self.suit, self.value))
+    
+    
+class Deck:
+    def __init__(self):
+        values = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"]
+        suits = ["diamonds", "hearts", "spades", "clubs"]
+        self.cards = [Card(value, suit) for value in values for suit in suits]
+    def __str__(self):
+        result = "["
+        for card in self.cards:
+            result += str(card) + ", "
+        result = result.removesuffix(", ")
+        result += "]"
+        return result
+    
+    def shuffle(self):
+        import random
+        random.shuffle(self.cards)
+
+    def draw_card(self):
+        return self.cards.pop()
+    
+
+class Hand:
+    def __init__(self, card):
+        self.cards = []
+        self.ace_count = 0
+        self.values = {"two" : 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10, "jack": 10, "queen": 10, "king": 10, "ace": 11}
+        self.add(card)
+        
+    def add(self, card):
+        if card.value == "ace":
+            self.ace_count += 1
+        self.cards.append(card)
+    
+    def calculate_value(self):
+        result_value = 0
+        for card in self.cards:
+            result_value += self.values[card.value]
+        if result_value > 21:
+            result_value -= self.ace_count*10
+        if result_value > 21:
+            return None
+        return result_value
+                
+            
+
+class Game:
+    def __init__(self):
+        self.deck = Deck()
+        self.deck.shuffle()
+        self.player_hand = Hand(self.deck.draw_card())
+        self.player_hand.add(self.deck.draw_card())
+        self.dealer_hand = Hand(self.deck.draw_card()) 
+        print("The Casinos card is: " + str(self.dealer_hand.cards[0]))
+        print()
+        while self.player_hand.calculate_value():
+            print("Your current hand is:")
+            for card in self.player_hand.cards:
+                print(card)
+            print("With a total value of:", self.player_hand.calculate_value())
+            user_input = input("Do you want to hit? (y/n)")   
+            if user_input == "y":
+                current_card = self.deck.draw_card()
+                self.player_hand.add(current_card)
+                print("You drew:", current_card)
+            elif user_input == "n":
+                print()
+                while self.dealer_hand.calculate_value() < 17 and self.dealer_hand.calculate_value() < self.player_hand.calculate_value():
+                    current_card = self.deck.draw_card()
+                    self.dealer_hand.add(current_card)
+                    print("Dealer drew:", current_card)
+                
+                dealer_value = self.dealer_hand.calculate_value()
+                player_value = self.player_hand.calculate_value()
+                print()
+                print("You have a value of", player_value)
+                print("The Casino has a value of", dealer_value)
+                if  dealer_value >= player_value:
+                    print("Therfore you lose. :(")
+                    return
+                else:
+                    print("Therfore you win.")
+                    print("Congratulations! :)")
+                    return 
+        print("You busted, therfore you lose. :(")
+        
+
+
+game = Game()
+    
+
+        
