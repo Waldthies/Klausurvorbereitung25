74425726efbab235fde142a292bd2f744786c64b
Waldthies <98609197+Waldthies@users.noreply.github.com>
2025-07-30 10:58:24 +0200

    Added "Fehler_Erkennung

---
+++ 1-Grundlagen/Fehler_Erkennung.py
@@ -0,0 +1,36 @@
+# Finde für jeden der Folgenden Code-Blöcke,
+# warum ein Fehler Erzeugt wird
+
+# a)
+
+#numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+#numbers_starting_with_letter_t = number for number in numbers if number[0] == "t"
+#print(numbers_starting_with_letter_t)
+
+
+# b)
+
+#r = range(5)
+#for i in r
+#    print(i)
+
+
+#c)
+
+#print(doubled(5))
+#def doubled(number):
+#    return number*2
+
+
+#d)
+
+#hello = 0.5*2
+#world = "Hello World"
+#print(hello * world)
+
+
+# e)
+
+#numbers = [1,2,3,4,5]
+#for i in range(numbers):
+#    print(i)
